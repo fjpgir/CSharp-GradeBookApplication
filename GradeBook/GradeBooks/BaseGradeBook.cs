e3b6a9bbd5b312c383b7355e4faa8d48b3bf4545
fjpgir <fjpgi@hotmail.com>
2019-02-08 04:37:59 -0500

    Update BaseGradeBook.cs
    Add Support for Weighted Grading

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -128,8 +128,7 @@ namespace GradeBook.GradeBooks
                     gpa = 0;
                     break;
             }
-            
-        
+                  
             if (IsWeighted && (studentType == StudentType.Honors || studentType== StudentType.DualEnrolled))
             {
                 gpa++;
