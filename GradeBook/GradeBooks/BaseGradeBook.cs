03eadefeb51ddbb6f154bf9335cee18e5bcf8c20
fjpgir <fjpgi@hotmail.com>
2019-02-12 12:14:08 -0500

    Revert "Update BaseGradeBook.cs"
    This reverts commit e3b6a9bbd5b312c383b7355e4faa8d48b3bf4545.

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -128,7 +128,8 @@ namespace GradeBook.GradeBooks
                     gpa = 0;
                     break;
             }
-                  
+            
+        
             if (IsWeighted && (studentType == StudentType.Honors || studentType== StudentType.DualEnrolled))
             {
                 gpa++;
