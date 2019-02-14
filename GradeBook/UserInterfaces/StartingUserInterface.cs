5dcde12637d9dd941dad84a6c433f83516d47395
fjpgir <fjpgi@hotmail.com>
2019-02-14 10:09:24 -0500

    Update StartingUserInterface.cs
    Add Support for Weighted Grading

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -39,7 +39,8 @@ namespace GradeBook.UserInterfaces
                 return;
             }
             var name = parts[1];
-            var type = parts[2].ToLower();
+            //var type = parts[2].ToLower();
+            var type = parts[2];
             var weighted = bool.Parse(parts[3]);
             BaseGradeBook gradeBook;
             if (type == "standard")
